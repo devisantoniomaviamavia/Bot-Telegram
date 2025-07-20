385af5dc5ee4844b0b9db34822c16345a0079100
devisantoniomaviamavia <devisantoniomaviamavia@gmail.com>
2025-07-20 10:58:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 from telegram.ext import Updater, CommandHandler
+from flask import Flask
+
+app = Flask(_name_)
 
 def start(update, context):
     update.message.reply_text("Olá! Bot funcionando.")
@@ -6,5 +9,11 @@ def start(update, context):
 updater = Updater("7999705337:AAHSVp2GmGxitTXGogm_7cwwTzr5tX51NHE", use_context=True)
 dp = updater.dispatcher
 dp.add_handler(CommandHandler("start", start))
-updater.start_polling()
-updater.idle()
+
+@app.route('/')
+def home():
+    return "Bot está online"
+
+if _name_ == '_main_':
+    updater.start_polling()
+    app.run(host='0.0.0.0', port=5000)
